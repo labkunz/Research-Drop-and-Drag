a1f423e9f49ea880e6de491119d021612819e49c
KentChen <bboykunz@gmail.com>
2023-06-15 19:46:49 +0800

    Test: Check pointer can be catch by pointer and console.log output the field

--- Brute-Forcing/adjust_size.js
+++ Brute-Forcing/adjust_size.js
@@ -1,6 +1,7 @@
-function RangeItem(start, end) {
+function RangeItem(start, end, index) {
     this.start = start;
     this.end = end;
+    this.index = index;
 
     this.getStart = function () {
         return this.start;
@@ -8,6 +9,10 @@ function RangeItem(start, end) {
 
     this.getEnd = function () {
         return this.end;
+    }
+
+    this.getIndex = function () {
+        return this.index;
     }
 }
 
@@ -17,17 +22,33 @@ document.addEventListener("DOMContentLoaded", function() {
     let rangeArray = [];
 
     let grids = document.querySelectorAll(".grid_mark");
+    let index = 1;
     grids.forEach(item => {
         let rect = item.getBoundingClientRect();
         let start = rect["x"];
         let end = start + rect["width"];
-        let obj = new RangeItem(start, end);
+        let obj = new RangeItem(start, end, index);
+        index++;
         rangeArray.push(obj);
     });
 
     //測試：console rangeArray item
     rangeArray.forEach(item => {
-        console.log(`item : start = ${item.start} / end = ${item.end}`);
+        console.log(`item ${item.getIndex()} : start = ${item.getStart()} / end = ${item.getEnd()}`);
     });
+
+    //測試：確認滑鼠可以抓到所在空間
+    let panel = document.querySelector(".container");
+    panel.addEventListener("pointermove", function(e) {
+
+        let current_page_x = e.pageX;
+        let checkArray = [...rangeArray];
+
+        checkArray.forEach(item => {
+            if (item.getStart() <= current_page_x && current_page_x <= item.getEnd()) {
+                console.log(`postion : fake_grid index = ${item.getIndex()}`);
+            }
+        })
+    })
 
 });
