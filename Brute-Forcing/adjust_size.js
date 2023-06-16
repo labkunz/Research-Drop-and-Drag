559fe48e05aa1264112ea73fcf5835728fd65d51
KentChen <bboykunz@gmail.com>
2023-06-16 16:30:13 +0800

    adjustWidth Ver.1: Need to record entry point info

--- Brute-Forcing/adjust_size.js
+++ Brute-Forcing/adjust_size.js
@@ -38,39 +38,75 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 
     //測試：確認滑鼠可以抓到所在空間
-    let panel = document.querySelector(".container");
-    panel.addEventListener("pointermove", function(e) {
+    // let panel = document.querySelector(".container");
+    // panel.addEventListener("pointermove", function(e) {
 
-        let current_page_x = e.pageX;
-        let checkArray = [...rangeArray];
+    //     let current_page_x = e.pageX;
+    //     let checkArray = [...rangeArray];
 
-        checkArray.forEach(item => {
-            if (item.getStart() <= current_page_x && current_page_x <= item.getEnd()) {
-                console.log(`postion : fake_grid index = ${item.getIndex()}`);
-            }
-        })
-    })
+    //     checkArray.forEach(item => {
+    //         if (item.getStart() <= current_page_x && current_page_x <= item.getEnd()) {
+    //             console.log(`postion : fake_grid index = ${item.getIndex()}`);
+    //         }
+    //     });
+    // });
 
     //建立resize區域並且增加相關事件
     const adjustWidth = (e) => {
+
+        console.log(e.pageX);
+
+        let current_page_x = e.pageX;
         let checkArray = [...rangeArray];
         let varietyWidth = 0;
 
-        checkArray.forEach(item => {
-            if (item.getStart() <= current_page_x && current_page_x <= item.getEnd()) {
+        // checkArray.forEach(item => {
+        //     if (item.getStart() <= current_page_x && current_page_x <= item.getEnd()) {
                 
-            } else {
-                let currentDistance = item.getEnd() - item.getStart();
-                varietyWidth += currentDistance;
+        //     } else {
+        //         let currentDistance = item.getEnd() - item.getStart();
+        //         varietyWidth += currentDistance;
+        //     }
+        // });
+
+        //確認目前在哪段區域
+        let getCurrentIndex = checkArray.find(item => {
+            if (item.getStart() <= current_page_x && current_page_x <= item.getEnd()) {
+                return item.getIndex();
             }
-        })
+        });
+
+        //把該段區域以前的長度加總起來
+        let getCurrentArray = checkArray.slice(getCurrentIndex);
+        let partPoint = 0;
+        getCurrentArray.forEach(item => {
+            let partWidth = item.getEnd() - item.getStart();
+            let partGap = item.getStart() - partPoint;
+            varietyWidth += (partGap + partWidth);
+            partPoint = item.getEnd();
+        });
+
+        //let card = document.querySelector(".resize_field")
+        let card = e.currentTarget.previousElementSibling;
+
+        card.style.width = `${getComputedStyle(card, null).width + varietyWidth}px`;
     }
 
     let resizeField = document.querySelector(".resize_field");
     resizeField.addEventListener("pointerdown", function(e) {
         //Can setting other element
 
+        console.log("input the pointerdown event");
+
         resizeField.addEventListener("pointermove", adjustWidth);
+    });
+
+    //當觸發pointup事件時,解除adjustWidth
+    resizeField.addEventListener("pointerup", function(e) {
+
+        console.log("input the pointerup event");
+
+        resizeField.removeEventListener("pointermove", adjustWidth);
     });
 
 });
